4e2636636e2be50374d87cdf73b530b0b39b8302
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-05-17 19:04:08 -0700

    add changes

--- functions.js
+++ functions.js
@@ -4,3 +4,53 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+function subtract(input1, input2) {
+   // A1: any Javascript value
+   // A2: any Javascript value
+   // R: a single JavaScript value
+   return input1 - input2;
+}
+function multiply(input1, input2) {
+   // A1: any Javascript value
+   // A2: any Javascript value
+   // R: a single JavaScript value
+   return input1 * input2;
+}
+function divide(input1, input2) {
+   // A1: any Javascript value
+   // A2: any Javascript value
+   // R: a single JavaScript value
+   return input1 / input2;
+}
+function increment(input) {
+   // A1: any Javascript numeric value
+   // R: a single JavaScript numeric value plus one
+   input++;
+   return input;
+}
+function decrement(input) {
+   // A1: any Javascript numeric value
+   // R: a single Javascript numeric value minus one
+   input--;
+   return input;
+}
+function decimalMultiply(input1, input2) {
+   // A1: any Javascript numeric decimal value
+   // A2: any Javascript numeric decimal value
+   // R: a single JavaScript numeric decimal value
+   return input1 * input2;
+}
+function decimalDivide(input1, input2) {
+   // A1: any Javascript numeric decimal value
+   // A2: any Javascript numeric decimal value
+   // R: a single JavaScript numeric decimal value
+   return input1 / input2;
+}
+function concatenationPlus(input1, input2) {
+   // A1: contents of first string
+   // A2: contents of second string
+   // R: returns two strings together, but with no space - need to fix
+   input1 = String(input1);
+   input1 + input2;
+   return input1;
+}
