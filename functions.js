41512e3cb2afd44ceee0e931acce2b221ddd675f
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-05-18 22:00:30 -0700

    add functions

--- functions.js
+++ functions.js
@@ -99,3 +99,53 @@ function findNthLastCharacter(input) {
    // return input1[input.length - input2];
    return input[input.length - 3];
 }
+function push(array, item) {
+   // A1: new Array is pushed to the end of the array
+   // R: array now has the new array at the end
+   array.push(item);
+   console.log(array);
+   return array;
+}
+function pop(array) {
+   // A1: target the end of the array to pop off
+   // R: array with end popped off
+   array.pop();
+   console.log(array);
+   return array;
+}
+function shift(array) {
+   // A1: remove first element of the array
+   // R: new array with new first element
+   array.shift();
+   console.log(array);
+   return array;
+}
+function unshift(array, addedValue) {
+   // A1: the array
+   // A2: the value we want to add at the beginning of the array
+   // R: new array with new first element
+   array.unshift(addedValue);
+   console.log(array);
+   return array;
+}
+
+var shoppingList = [];
+
+function makeshoppingList(item, quantity) {
+   // A1: a new item and quantity begins the shopping list(var shopping list declared above=array)
+   // A2: each new item and quantity is added to the end of the list
+   // R: a shopping list with items and quantities
+   shoppingList.push([item, quantity]);
+   console.log(shoppingList);
+   return shoppingList;
+}
+
+function nextInLine(arr, item) {
+   // A1: a new item is pushed to the end of the array
+   // A2: the first item in the array is shifted off the array
+   // R: a new array less the original first element
+   arr.push(item);
+   console.log(arr);
+   arr.shift();
+   return arr;
+}
